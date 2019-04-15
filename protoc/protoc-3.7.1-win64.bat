f401a8c269a87485f432c51441b9b0b5c8a9c0e4
qq2248022127 <2248022127@qq.com>
2019-04-15 18:29:02 +0800

    [update]测试..

--- protoc/protoc-3.7.1-win64.bat
+++ protoc/protoc-3.7.1-win64.bat
@@ -1,2 +1,5 @@
-.\protoc-3.7.1-win64.exe --go_out=..\src\ .\proto\gs\gs.proto
+rem grpc版协议
+.\protoc-3.7.1-win64.exe --go_out=plugins=grpc:..\src\ .\proto\gs\gs.proto
+rem 标准protobuf
+rem .\protoc-3.7.1-win64.exe --go_out=..\src\ .\proto\gs\gs.proto
 pause
